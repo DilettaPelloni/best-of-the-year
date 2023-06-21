1354a942c615d3f86dcf8aa789812cb25db13061
Diletta Pelloni <dilpel@live.it>
2023-06-21 15:03:33 +0200

    fatta rotta dettaglio song

--- src/main/java/org/lessons/bestOfTheYear/controller/SongController.java
+++ src/main/java/org/lessons/bestOfTheYear/controller/SongController.java
@@ -1,10 +1,12 @@
 package org.lessons.bestOfTheYear.controller;
 
 import org.lessons.bestOfTheYear.model.Media;
+import org.lessons.bestOfTheYear.model.Movie;
 import org.lessons.bestOfTheYear.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +21,29 @@ public class SongController {
         return "songs";
     }
 
+    @GetMapping("/{id}")
+    public String movieDetail(
+            Model model,
+            @PathVariable int id
+    ) {
+        Song song = null;
+        for (Song s : getBestSongs()) {
+            if(s.getId() == id) {
+                song = s;
+            }
+        }
+
+        if(song != null) { //se l'id è valido mi porta alla pagina di dettaglio
+            model.addAttribute("song", song);
+            return "songDetail";
+        } else { //altrimenti mi reindirizza alla home
+            return "redirect:/";
+        }
+    }
+
     //METODI PRIVATI ----------------------------------------------------------------------
-    private List<Media> getBestSongs() {
-        List<Media> songs = new ArrayList<>();
+    private List<Song> getBestSongs() {
+        List<Song> songs = new ArrayList<>();
         songs.add(new Song(1, "Love of my life"));
         songs.add(new Song(2, "Seven seas of Rhye"));
         songs.add(new Song(3, "Hammer to fall"));
